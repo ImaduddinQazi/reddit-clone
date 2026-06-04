41dc47ece2045c53043337e24fe46e01153749c1
Imaduddin Qazi <155837474+ImaduddinQazi@users.noreply.github.com>
2026-06-04 21:07:44 +0530

    Prisma setup

--- src/app/r/[name]/page.tsx
+++ src/app/r/[name]/page.tsx
@@ -1,11 +1,3 @@
-// export default async function SubRedditPAge({ params }: { params: Promise<{name: string}>}){
-//     const {name} = await params;
-//     return (
-//         <h1>this is {name}</h1>
-//     )
-
-// }
-
 export default async function SubRedditPage({ params}: { params: Promise <{name: string}>}){
     const {name} = await params;
     return (
