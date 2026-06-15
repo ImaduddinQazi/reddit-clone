0a00b95bb615dfca3e7d2e2a64a3a1af49eccf74
Imaduddin Qazi <155837474+ImaduddinQazi@users.noreply.github.com>
2026-06-15 23:59:49 +0530

    posts POST method

--- src/app/api/posts/route.ts
+++ src/app/api/posts/route.ts
@@ -4,3 +4,16 @@ export async function GET(){
     const data= await prisma.post.findMany();
     return Response.json(data);
 }
+export async function POST(request: Request){
+    const body = await prisma.post.create({
+        data:{
+            userId: prisma.userId,
+            title: prisma.title,
+            image: prisma.image,
+            description: prisma.description,
+            communityId: prisma.communityId
+        }
+    })
+    
+    return Response.json(body);
+}
